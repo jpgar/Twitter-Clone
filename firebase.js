8516a09a4d2a64731fce56dcb7a77b4efe0204b3
jpgar <110881536+jpgar@users.noreply.github.com>
2022-08-18 16:19:12 -0400

    finished sign in page

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: "twitter-clone-15a22.firebaseapp.com",
     projectId: "twitter-clone-15a22",
     storageBucket: "twitter-clone-15a22.appspot.com",
