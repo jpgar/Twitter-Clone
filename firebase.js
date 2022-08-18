5c166d1ff56e87dd0c9b056f940ce93f76fd64c2
jpgar <110881536+jpgar@users.noreply.github.com>
2022-08-18 14:24:20 -0400

    finished installing next-auth and  firebase

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: "twitter-clone-15a22.firebaseapp.com",
+    projectId: "twitter-clone-15a22",
+    storageBucket: "twitter-clone-15a22.appspot.com",
+    messagingSenderId: "318609134851",
+    appId: "1:318609134851:web:179a325a7420dcda330536"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApps();
+const db = getFirestore();
+const storage = getStorage();
+export { app, db, storage };
