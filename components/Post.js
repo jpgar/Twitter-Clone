9af97a58dda7d167d1e0c2ad4d6239cedd1efc9b
jpgar <110881536+jpgar@users.noreply.github.com>
2022-08-21 15:26:17 -0400

    finished react-modal installing and styling

--- components/Post.js
+++ components/Post.js
@@ -7,7 +7,7 @@ import { signIn, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { deleteObject, ref } from "firebase/storage";
 import { useRecoilState } from "recoil";
-import { modalState } from "../atom/modalAtom";
+import { modalState, postIdState } from "../atom/modalAtom";
 
 
 export default function Post({ post }) {
@@ -15,6 +15,7 @@ export default function Post({ post }) {
     const [likes, setLikes] = useState([]);
     const [hasLiked, setHasLiked] = useState(false);
     const [open, setOpen] = useRecoilState(modalState);
+    const [postId, setPostId] = useRecoilState(postIdState);
     useEffect(() => {
         const unsubscribe = onSnapshot(
             collection(db, "posts", post.id, "likes"), (snapshot) => setLikes(snapshot.docs)
@@ -56,7 +57,7 @@ export default function Post({ post }) {
             {/* user image */}
             <img
                 className="h-11 w-11 rounded-full mr-4"
-                src={post?.data()?.userImg}
+                src={post.data().userImg}
                 alt="user-img"
             />
             {/* right */}
@@ -68,10 +69,10 @@ export default function Post({ post }) {
 
                     {/* user info */}
                     <div className="flex items-center space-x-1 whitespace-nowrap">
-                        <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">{post?.data()?.name}</h4>
-                        <span className="text-sm sm:text-[15px] ">@{post?.data()?.username} - </span>
+                        <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">{post.data().name}</h4>
+                        <span className="text-sm sm:text-[15px] ">@{post.data().username} - </span>
                         <span className="text-sm sm:text-[15px] hover:underline">
-                            <Moment fromNow>{post?.data()?.timestamp?.toDate()}</Moment>
+                            <Moment fromNow>{post.data().timestamp?.toDate()}</Moment>
                         </span>
                     </div>
                     {/* dot icon */}
@@ -80,16 +81,25 @@ export default function Post({ post }) {
 
                 {/* post text */}
 
-                <p className="text-gray-800 text-[15px] sm:text-[16px] mb-2">{post?.data()?.text}</p>
+                <p className="text-gray-800 text-[15px] sm:text-[16px] mb-2">{post.data().text}</p>
                 {/* post img */}
 
-                <img src={post?.data()?.image} alt="" className="rounded-2xl mr-2" />
+                <img src={post.data().image} alt="" className="rounded-2xl mr-2" />
 
                 {/* icons */}
 
                 <div className="flex justify-between text-gray-500 p-2 ">
 
-                    <ChatIcon onClick={() => setOpen(!open)} className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+                    <ChatIcon onClick={() => {
+                        if (!session) {
+                            signIn();
+                        } else {
+                            setPostId(post.id)
+                            setOpen(!open)
+                        }
+                    }
+                    }
+                        className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
                     {session?.user.uid === post?.data().id && (
                         <TrashIcon onClick={deletePost} className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
                     )}
